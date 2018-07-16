f48048bfec301a81274cce91cece4871baeaaf9a
Łukasz <l.parysek@gmail.com>
2018-07-16 23:14:19 +0200

    Added validation example

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -1,41 +1,65 @@
 
 <template>
     <div>
-        <input ref="autocomplete"
-               placeholder="Search"
-               class="search-location"
-               onfocus="value = ''"
-               type="text" />
+        <input v-model="$v.text.$model" :class="status($v.text)">
+        <div class="btn-danger" v-if="!$v.text.required && $v.text.$dirty">Field is required</div>
+        <div class="btn-danger" v-if="!$v.text.minLength && $v.text.$dirty">Field is minLength</div>
+        <pre>{{ $v }}</pre>
     </div>
-
 </template>
 
 <script>
-    export default {
-        name: "Location",
-        data () {
-          return {
-              place: {
-              }
-          }
-        },
-        mounted() {
-            this.autocomplete = new google.maps.places.Autocomplete(
-                (this.$refs.autocomplete),
-                {types: ['geocode']}
-            );
+    import { validationMixin } from 'vuelidate'
+    import { required, minLength } from 'vuelidate/lib/validators'
 
-            this.autocomplete.addListener('place_changed', () => {
-                this.place = this.autocomplete.getPlace();
-                this.place.ac = this.place.address_components;
-                let lat = this.place.geometry.location.lat();
-                let lon = this.place.geometry.location.lng();
-                let city =  this.place.ac[0]["short_name"];
-            });
+    export default {
+        data () {
+            return {
+                text: ''
+            }
+        },
+        mixins: [validationMixin],
+        validations: {
+            text: {
+                required,
+                minLength: minLength(5)
+            }
+        },
+        methods: {
+            status(validation) {
+                return {
+                    error: validation.$error,
+                    dirty: validation.$dirty
+                }
+            }
         }
     }
 </script>
 
 <style scoped>
+    input {
+        border: 1px solid silver;
+        border-radius: 4px;
+        background: white;
+        padding: 5px 10px;
+    }
+
+    .dirty {
+        border-color: #5A5;
+        background: #EFE;
+    }
+
+    .dirty:focus {
+        outline-color: #8E8;
+    }
+
+    .error {
+        border-color: red;
+        background: #FDD;
+    }
+
+    .error:focus {
+        outline-color: #F99;
+    }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ Vue.use(BootstrapVue);
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
